e7bbc275b60c9beb1048a497b0f3fb6cf226ae41
unknown <laxmipriya.padegal2023@vitstudent.ac.in>
2026-01-12 02:17:42 +0530

    Remove detectBot from middleware to reduce bundle size

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
-import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
+import arcjet, { createMiddleware, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -17,14 +17,7 @@ const aj = arcjet({
     shield({
       mode: "LIVE",
     }),
-    detectBot({
-      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
-      allow: [
-        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-        "GO_HTTP", // For Inngest
-        // See the full list at https://arcjet.com/bot-list
-      ],
-    }),
+    // detectBot removed to reduce middleware bundle size for Vercel deployment
   ],
 });
 
